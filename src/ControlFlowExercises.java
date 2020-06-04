e2ae957abbf2042d864cb430e5e82c44d5221e15
trevormullis12 <trevor.mullis12@gmail.com>
2020-06-04 10:08:02 -0500

    table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 0; i <= 100; i++) {
-            System.out.print(i + " ");
-            if(i % 3 == 0)
-                System.out.print("Fizz");
-            if(i % 5 == 0)
-                System.out.print("Buzz");
-            System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer to go up to: ");
+        int integer = scanner.nextInt();
+        System.out.println("Here is your table:\n");
+        System.out.println("number | squared | cubed");
+        System.out.println("-------|---------|-------");
+
+        for(int i = 1; i <= integer; i++) {
+            System.out.print(i);
+            System.out.print("      | ");
+            System.out.print(i * i);
+            System.out.print("       | ");
+            System.out.println(i * i * i);
         }
     }
 }
