d68a803eeb82c25a7552df61eadbba84a7b4db63
trevormullis12 <trevor.mullis12@gmail.com>
2020-06-08 11:37:26 -0500

    methods exercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,25 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        add(1, 2);
+        subtract(5, 2);
+        multiply(25, 4);
+        divide(8, 4);
+        mod(24, 2);
+    }
+
+    public static void add(int num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+    public static void subtract(int num1, int num2) {
+        System.out.println(num1 - num2);
+    }
+    public static void multiply(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+    public static void divide(int num1, int num2) {
+        System.out.println(num1 / num2);
+    }
+    public static void mod(int num1, int num2) {
+        System.out.println(num1 % num2);
+    }
+}
