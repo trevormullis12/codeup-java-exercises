5b0f5cd5623406483335daec5a38a79746a929ef
trevormullis12 <trevor.mullis12@gmail.com>
2020-06-08 13:00:10 -0500

    finished HighLow

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class HighLow {
-}
+        public static void main(String[] args) {
+            int randomNum = (int)Math.ceil(Math.random() * 100);
+            numberGuess(randomNum);
+        }
+
+        public static void numberGuess(int num) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a random number between 1 and 100: ");
+            int guess = sc.nextInt();
+
+            System.out.println("Is this your answer? (y/n)");
+            String input = sc.next();
+            if (input.equalsIgnoreCase("y")) {
+                if (guess > num) {
+                    System.out.println("LOWER");
+                    numberGuess(num);
+                } else if (guess < num) {
+                    System.out.println("HIGHER");
+                    numberGuess(num);
+                } else if (guess == num) {
+                    System.out.println("GOOD GUESS");
+                }
+            } else {
+                System.out.println("ok bye!");
+            }
+        }
+    }
